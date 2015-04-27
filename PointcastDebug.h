f5a5e9b32913e6f41a47a2d282ff5a246662b2fe
robouden <rob@yr-design.biz>
2015-04-27 12:28:47 +0900

    More files

---
+++ PointcastDebug.h
@@ -0,0 +1,15 @@
+#ifndef _Pointcast_DEBUG_H_
+#define _Pointcast_DEBUG_H_
+
+#define ENABLE_DEBUG 
+
+// Debug definitions ----------------------------------------------------------
+#ifdef ENABLE_DEBUG
+  #define DEBUG_PRINT(x) Serial.print(x)
+  #define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTLN(x)
+#endif
+
+#endif
