e425c03ba62705ec76ade1ad88fc6af850e399d3
robouden <rob@yr-design.biz>
2015-05-24 20:28:38 +0900

    And the files

---
+++ lib/a3gim_uno/examples/set_baudrate/set_baudrate.ino
@@ -0,0 +1,47 @@
+// A3GS sample sketch -- setBaudrate
+//
+//   You need to use "setBaudrate()" function CAREFULLY. 
+//   Because, 3G Shield becomes impossible to use when setting up 
+//   the value which is not suitable. 
+//   This function is used when gathering the communication speed of 
+//   Arruino and 3G Shield using HardwareSerial instead of SoftwareSerial.
+//   In order for you to use HardwareSerial, you need to correct "a3gs.h" 
+//   and "a3gim.cpp" appropriately.
+//   When using an "a3gim" library after performing this sketch, 
+//   begin() is called in the following arguments: 
+//       begin(0, 2400);
+
+#include <SoftwareSerial.h>
+#include "a3gim.h"
+
+#define NEW_BAUDRATE  19200
+//@ #define NEW_BAUDRATE  9600
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(3000);  // Wait for Start Serial Monitor
+  Serial.println("Ready.");
+
+  Serial.print("Initializing.. ");
+  if (a3gs.start() == 0 && a3gs.begin() == 0) {
+//@  if (a3gs.start() == 0 && a3gs.begin(0, 2400) == 0) {
+    Serial.println("Succeeded.");
+    if (a3gs.setBaudrate(NEW_BAUDRATE) == 0) {
+      Serial.print("Baudrate was changed as ");
+      Serial.print(NEW_BAUDRATE);
+      Serial.println(" bps.");
+    }
+  }
+  else
+    Serial.println("Failed.");
+
+  Serial.println("Shutdown..");
+  a3gs.end();
+  a3gs.shutdown();
+}
+
+void loop()
+{
+}
+
---
+++ lib/a3gim_uno/examples/tweet_sample/tweet_sample.ino
@@ -0,0 +1,37 @@
+// A3GS sample sketch.10 -- tweet
+//  [[Note]] REPLACE "token" and "message" WITH YOUR GOT TOKEN AND MESSAGE BEFORE UPLOAD THIS SKETCH.
+//           THIS TEST SKETCH USE http://arduino-tweet.appspot.com/ 's SERVICE. PLEASE CHECK DETAIL IN THIS SITE.
+
+#include <SoftwareSerial.h>
+#include "a3gim.h"
+
+const char *token = "YOUR_TOKEN_HERE";
+const char *message = "TWEET_MESSAGE_HERE";
+  //-- Note: can't tweet same message continuously.
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(3000);  // Wait for Start Serial Monitor
+  Serial.println("Ready.");
+
+  Serial.print("Initializing.. ");
+  if (a3gs.start() == 0 && a3gs.begin() == 0) {
+    Serial.println("Succeeded.");
+    Serial.print("tweet() requesting.. ");
+    if (a3gs.tweet(token, message) == 0)
+      Serial.println("OK!");
+    else
+      Serial.println("Can't tweet.");
+  }
+  else
+    Serial.println("Failed.");
+
+  Serial.println("Shutdown..");
+  a3gs.end();
+  a3gs.shutdown();
+}
+
+void loop()
+{
+}
